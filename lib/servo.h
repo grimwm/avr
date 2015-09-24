fbd88f666dbef3aedaa7b74cc738351635a80dbf
William Grim <grimwm@gmail.com>
2015-09-23 20:00:03 -0500

    Reorganize the code and make way for TCNT0-based servos.

--- lib/servo.h
+++ lib/servo.h
@@ -4,17 +4,19 @@
 extern "C" {
 #endif
 
+#include <avr/interrupt.h>
+
 #include "pwm.h"
 
 #define PWM_MIN_US 500
 #define PWM_MAX_US 1000
 
 /**
- * Generate PWM for servo positions between 0 and 180 degrees.
+ * Generate PWM duty cycle for servo positions between 0 and 180 degrees.
  */
-static inline void servo(unsigned degrees, volatile uint16_t* pin) {
+static inline unsigned servo(unsigned degrees) {
   unsigned us = PWM_MIN_US + ((PWM_MAX_US - PWM_MIN_US) * degrees / 180);
-  *pin = us_clocks(us, Prescaled_8);
+  return us_clocks(us, Prescaled_8);
 }
 
 /**
@@ -54,16 +56,36 @@ static inline void servo1_init(unsigned out_pins) {
   ICR1 = us_clocks(10000, Prescaled_8);
 
   // Set servos to 90 degrees.
-  servo(90, &OCR1A);
-  servo(90, &OCR1B);
+  OCR1A = OCR1B = servo(90);
 }
 
 /**
- * Install a set of ISRs for managing 50 Hz servo signals at the proper
- * duty cycles.  use the associated non-ISR methods to set the
- * appropriate registers for the desired duty cycles.
+ * If INSTALL SERVO0_ISRS is set, install a set of ISRs for managing
+ * 50 Hz servo signals at the proper duty cycles.  use the associated
+ * non-ISR methods to set the appropriate registers for the desired
+ * duty cycles.
  */
 #ifdef INSTALL_SERVO0_ISRS
+extern volatile uint8_t x200us;
+/**
+ * Set this up with the assumption we have a TOP of 200,
+ * with each tick counting as 1us.  So, when this has executed
+ * exactly 100 times, it will have been 100*200us = 20ms or 50 Hz,
+ * enough for a servo signal period.
+ */
+ISR(TIMER0_OVF_vect) {
+  ++x200us;
+  if (100 == x200us) { // clock-cycle efficient
+    // We've reached 20ms and need to reset.
+    x200us = 0;
+  }
+}
+
+ISR(TIMER0_COMPA_vect) {
+}
+
+ISR(TIMER0_COMPB_vect) {
+}
 #endif // INSTALL_SERVO0_ISRS
 
 #ifdef __cplusplus
--- pwm.c
+++ pwm.c
@@ -1,6 +1,5 @@
 #include <inttypes.h>
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #include <util/delay.h>
 
 #include "servo.h"
@@ -8,31 +7,11 @@
 
 #define ACK 0x7E
 
-// These servo ISRs will be moved into servo.h, hidden behind a
-// macro definition.
-extern volatile int8_t x200us;
-volatile int8_t x200us;
-/**
- * Set this up with the assumption we have a TOP of 200,
- * with each tick counting as 1us.  So, when this has executed
- * exactly 100 times, it will have been 100*200us = 20ms or 50 Hz,
- * enough for a servo signal period.
- */
-ISR(TIMER0_OVF_vect) {
-  ++x200us;
-  if (100 == x200us) { // clock-cycle efficient
-    x200us = 0;
-  }
-}
-
-ISR(TIMER0_COMPA_vect) {
-}
-
-ISR(TIMER0_COMPB_vect) {
-}
+// Memory for the ISR
+volatile uint8_t x200us;
 
 int main (void) {
-  servo0_init(OC1A | OC1B);
+  servo0_init(OC0A | OC0B);
   spi_init_slave(0);
   sei();
 
@@ -40,12 +19,9 @@ int main (void) {
   for (;;) {
   /*   sleep_mode(); // sleep until we're awoken by an interrupt */
     for (unsigned char degrees = 0; degrees <= 180; ++degrees) {
-    servo(degrees, &OCR1A);
-    servo(degrees, &OCR1B);
+      /* _delay_ms(1000); */
 
-    /* _delay_ms(1000); */
-
-    degrees = spi_tranceiver(ACK);
+      degrees = spi_tranceiver(ACK);
     }
   }
 
