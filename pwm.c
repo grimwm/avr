b5853ea56bacc1f9e24d80c8e1aa2df02eaa5bdc
William Grim <grimwm@gmail.com>
2015-09-26 03:59:40 -0500

    Quite a bit of code added for the PWM, combined with use of the UART.  Still some bugs, but it sort of works.

--- pwm.c
+++ pwm.c
@@ -3,18 +3,48 @@
 #include <util/delay.h>
 
 #include "servo.h"
+#include "pwm.h"
 #include "uart.h"
 
 int main (void) {
-//  servo_init(OC1A | OC1B);
+  cs1(Prescaled_8);
+  wgm1(PhaseCorrectPWM);
+  oc1(NonInverting);
+  oc1_enable(OC1A | OC1B);
+
   uart_enable(UM_Asynchronous);
   sei();
 
-//  unsigned char degrees = 90;
+  /* volatile uint16_t* pin = 0; */
+  uint16_t dutyBuffer = 0;
+  int doEcho = 0;
   for (;;) {
-  /*   sleep_mode(); // sleep until we're awoken by an interrupt */
-    for (unsigned char degrees = 0; degrees <= 180; ++degrees) {
-      _delay_ms(1000);
+    unsigned char b = uart_receive();
+    if (b >= '0' && b <= '9') {
+      dutyBuffer *= 10;
+      dutyBuffer += b-'0';
+    }
+
+    if ('\r' == b) {
+      /* if (pin) { */
+      /*   *pin = dutyBuffer; */
+      /* } */
+      OCR1A = dutyBuffer;
+      while (dutyBuffer > 0) {
+        b = (dutyBuffer % 10) + '0';
+        dutyBuffer /= 10;
+      }
+      b = '\r';
+    /* } else if ('A' == b) { */
+    /*   pin = &OCR1A; */
+    /* } else if ('B' == b) { */
+    /*   pin = &OCR1B; */
+    } else if ('E' == b || 'e' == b) {
+      doEcho = !doEcho;
+    }
+
+    if (doEcho) {
+      uart_transmit(b);
     }
   }
 
