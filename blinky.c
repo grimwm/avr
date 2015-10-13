a55bbe49efbcc4c9bf40364162de643b31d9b6cc
William Grim <grimwm@gmail.com>
2015-10-13 17:37:19 -0500

    Standardize blinky to PORTB, which is where we also test our PWMs.

--- blinky.c
+++ blinky.c
@@ -13,23 +13,23 @@
 
 int main(void)
 {
-  DDRD = 0xFF;  // PORTB is output, all pins
-  PORTD = 0x00; // Make pins low to start
+  DDRB = 0xFF;  // PORTB is output, all pins
+  PORTB = 0x00; // Make pins low to start
 
   // blink an LED for S.O.S.
   for (;;) {
-    PORTD ^= 0xFF;   // invert all the pins
+    PORTB ^= 0xFF;   // invert all the pins
     for (int i = 0; i < 3; ++i) {
-      PORTD = 0xFF;
+      PORTB = 0xFF;
       _delay_ms(200);
-      PORTD = 0x00;
+      PORTB = 0x00;
       _delay_ms(200);
     }
 
     for (int i = 0; i < 3; ++i) {
-      PORTD = 0xFF;
+      PORTB = 0xFF;
       _delay_ms(1000);
-      PORTD = 0x00;
+      PORTB = 0x00;
       _delay_ms(200);
     }
   }
